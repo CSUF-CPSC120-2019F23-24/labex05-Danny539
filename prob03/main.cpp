96607b06b4940a776731504ef7696dce163d4c7a
danny539 <danny539@csu.fullerton.edu>
2019-09-21 14:44:32 -0700

    Prob03-Danny539

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,26 @@
 // Random Rectangle
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <ctime>
+int main()
+{
+  //planted a string and put an interger max at 40 so that it wouldn't exceeed that
+  // And the seed made it so that it was continuously Random
+  unsigned seed = time(0);
+  srand(seed);
+  std::string numsign;
+  int max = 40;
+  int num;
+  num = rand() % max + 1;
+
+  numsign.assign(num,'#');
+// this is where I repeated the same line 3 times so that it would be 3 high
+  std::cout << numsign << std::endl;
+  std::cout << numsign << std::endl;
+  std::cout << numsign << std::endl;
+
+return 0;
+
+}
